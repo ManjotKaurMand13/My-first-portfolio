afdc920e4d60445db6d4816f6bfae6558ac96908
Manjot Kaur Mand <manjotkaurmand1313@gmail.com>
2022-03-12 11:35:54 -0600

    toggle button updated

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,1 @@
-window.onload=()=>{document.getElementById("top").addEventListener("click",()=>{document.documentElement.scrollTop=0})};
+window.onload=()=>{document.getElementById("top").addEventListener("click",()=>{document.documentElement.scrollTop=0})};function toggle(){document.body.classList.toggle("dark")}
